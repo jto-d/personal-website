db5c386aeeeef262323b04be1838f7a7f643e264
Jack <90474780+jto-d@users.noreply.github.com>
2022-07-28 23:20:24 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import './App.scss'
+import { About, Footer, Header, Skills, Work } from './container'
+import { Navbar } from './components'
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Footer />
+
+
+    </div>
+  );
+}
+
+export default App;
