24e97060fca00b65fe705e30a2e98548cc2fe1df
Jack O'Donnell <jodonnell@Jacks-MBP.fios-router.home>
2022-07-31 00:12:06 -0400

    Updated Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { HiMenuAlt4, Hix } from 'react-icons/hi'
+import { motion } from 'framer-motion'
 
 import './Navbar.scss'
 
@@ -6,17 +8,21 @@ const Navbar = () => {
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        <span>Jack.</span>
+        <span>J</span>
         
       </div>
       <ul className="app__navbar-links">
         {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
-            <div />
             <a href={`#${item}`}>{item}</a>
           </li>
         ))}
       </ul>
+
+      <div className="app__navbar-menu">
+
+
+      </div>
     </nav>
   )
 }
