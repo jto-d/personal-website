0655c6a1ff1579519dcd82c42b2ff9b0cafaa555
Jack O'Donnell <jodonnell@Jacks-MBP.lan>
2022-08-01 01:16:47 -0400

    updated navbar, implement hide on scroll

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import { HiMenuAlt4, Hix } from 'react-icons/hi'
+import React, { useState } from 'react'
+import { HiMenuAlt4, HiX } from 'react-icons/hi'
 import { motion } from 'framer-motion'
 
 import './Navbar.scss'
 
 const Navbar = () => {
+
+  const [toggle, setToggle] = useState(false);
   return (
     <nav className="app__navbar">
+
+      {/* ${props =>
+      props.scrollDirection === 'up' &&
+        !props.scrolledToTop &&
+        CSS 
+          transform: translateY(0px);
+          box-shadow: 0 10px 30px -10px var(--dark-shadow);
+        
+        
+        } */}
+
       <div className="app__navbar-logo">
         <span>J</span>
         
@@ -20,7 +33,23 @@ const Navbar = () => {
       </ul>
 
       <div className="app__navbar-menu">
+          <HiMenuAlt4 onClick={() => setToggle(true)}/>
 
+          {toggle && (
+            <motion.div
+              whileInView={{ x: [300,0] }}
+              transition={{ duration: 0.35, ease: 'easeOut'}}
+            >
+              <HiX onClick={() => setToggle(false)}/>
+              <ul>
+                {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                  <li key={item}>
+                    <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
+                  </li>
+                ))}
+              </ul>
+            </motion.div>
+          )}
 
       </div>
     </nav>
