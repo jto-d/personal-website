ca6a4ec66ee07ff9f472e9c47cead8094bf82c3e
Jack <90474780+jto-d@users.noreply.github.com>
2024-10-03 02:24:23 -0400

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  flex-direction: column;
+  display: flex;
+`
+
+function App() {
+
+  return (
+    <Container>
+      <p>Test text</p>
+    </Container>
+  )
+}
+
+export default App
